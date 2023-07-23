0099136f493540184ec7f574a40e1a25ba53e825
Mahmoud Almalah <75377167+MahmoudAlmalah-ENG@users.noreply.github.com>
2023-07-23 15:20:03 +0300

    add payment

--- app/Http/Controllers/Api/Payment.php
+++ app/Http/Controllers/Api/Payment.php
@@ -24,4 +24,39 @@ class Payment extends Controller
             ]);
         }
     }
+
+    public function post()
+    {
+        try {
+            $pay = new PaymentModel();
+            $pay->name = request()->name;
+            $pay->status = request()->status;
+            $pay->type = request()->type;
+            $images = array();
+            if ($files = request()->image) {
+                foreach ($files as $file) {
+                    $image_name = md5(rand(100, 200));
+                    $ext = strtolower($file->getClientOriginalExtension());
+                    $image_full_name = $image_name . '.' . $ext;
+                    $upload_path = 'images/payments/';
+                    $image_url = $upload_path . $image_full_name;
+                    $file->move($upload_path, $image_full_name);
+                    $images[] = $image_url;
+                }
+            }
+            $pay->image = $images;
+            $pay->save();
+            return response()->json([
+                'status' => true,
+                'message' => 'تم اضافة البيانات بنجاح',
+                'data' => $pay
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'حدث خطأ ما',
+                'data' => $e->getMessage()
+            ]);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -85,6 +85,7 @@ Route::group(['middleware' => ['api']], function () {
         Route::post('/changePassword', [AuthController::class, 'changePassword']);
         Route::get('/splach', [HomeScreenController::class, 'splach']);
         Route::get('/payment', [\App\Http\Controllers\Api\Payment::class, 'index']);
+        Route::post('/payment', [\App\Http\Controllers\Api\Payment::class, 'post']);
 
     }
 });
